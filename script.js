48fca1acdfbac52c765556234599c78459f767d6
Paddedgoddess <goddesspadded@gmail.com>
2024-04-09 11:53:17 +0200

    else_if

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+
+
+
+const inputValue = document.querySelector("#inputValue").value;
+console.log(inputValue);
+
+const submitValue = document.querySelector("#submitValue");
+console.log(submitValue);
+
+submitValue.addEventListener("click", () => {
+    const inputValue = document.querySelector("#inputValue").value;
+console.log(inputValue);
+
+const displayValue = document.querySelector("#displayValue");
+console.log(displayValue);
+
+displayValue.textContent = inputValue;
+})
+
---
+++ script2.js
@@ -0,0 +1,27 @@
+// if/else statement
+
+
+let number = 1;
+
+// = erklrer en verdi
+// == sjekker innhold
+// === sjekker innhold og datatype
+// & og / and 
+// | eller / either
+
+
+if (number === 10) {
+console.log("dette tallet er akkurat 10");
+}
+
+else if (number < 10) {
+console.log("dette tallet er mindre enn 10")
+}
+
+else if (number > 10) {
+console.log("dette tallet er større enn 10")
+}
+
+else {
+console.log("dette tallet er ikke 10");
+}
