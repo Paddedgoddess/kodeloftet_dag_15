9d22c77b5a76190602130cff08ad020a5ef77e3a
Paddedgoddess <goddesspadded@gmail.com>
2024-04-11 12:09:18 +0200

    nesten ferdig

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 
 
-const inputValue = document.querySelector("#inputValue").value;
-console.log(inputValue);
-
 const submitValue = document.querySelector("#submitValue");
 console.log(submitValue);
 
@@ -15,5 +12,39 @@ const displayValue = document.querySelector("#displayValue");
 console.log(displayValue);
 
 displayValue.textContent = inputValue;
-})
+
+const fromValue = document.querySelector("#fromValue").value;
+console.log(fromValue)
+
+const toValue = document.querySelector("#toValue").value;
+console.log(toValue)
+
+let result;
+
+
+if (fromValue === "CM" & toValue === "M") {
+result = inputValue / 100;
+}
+else if (fromValue === "CM" & toValue === "KM") {
+    result = inputValue / 100000;
+}
+else if (fromValue === "M" & toValue === "CM") {
+    result = inputValue * 100;
+}
+else if (fromValue === "M" & toValue === "KM") {
+    result = inputValue / 1000;
+}
+
+else if (fromValue === "KM" & toValue === "CM") {
+    result = inputValue * 100000;
+}
+else if (fromValue === "KM" & toValue === "M") {
+   result = inputValue * 1000 
+}
+else {
+    console.log("Please choose two different options.");
+}
+
+displayValue.textContent = result;
+});
 
